2e4174f1f2550c728f7c0030f232a9a913dcb9f8
Xiaojiu-z <97741514+Xiaojiu-z@users.noreply.github.com>
2025-04-11 02:22:10 +0800

    Update train_spatial.sh

--- train/train_spatial.sh
+++ train/train_spatial.sh
@@ -1,6 +1,4 @@
-# export MODEL_DIR="black-forest-labs/FLUX.1-dev" # your flux path
-export MODEL_DIR="/opt/liblibai-models/model-weights/black-forest-labs/FLUX.1-dev" # your flux path
-
+export MODEL_DIR="black-forest-labs/FLUX.1-dev" # your flux path
 export OUTPUT_DIR="./models/pose_model"  # your save path
 export CONFIG="./default_config.yaml"
 export TRAIN_DATA="./examples/pose.jsonl" # your data jsonl file
