2c3528fa624392d885bef388456e0bc0ca11242a
zhangsongbo <zhangp365@gmail.com>
2025-05-08 16:39:23 +0800

    update the predict.py for pose and mine_craft controls.

--- predict.py
+++ predict.py
@@ -9,11 +9,11 @@ from PIL import Image
 from typing import List
 from cog import BasePredictor, Input, Path
 from huggingface_hub import hf_hub_download
-
+import yaml
 # Assuming src is in the same directory or PYTHONPATH is set correctly
 from src.pipeline import FluxPipeline
 from src.transformer_flux import FluxTransformer2DModel
-from src.lora_helper import set_single_lora
+from src.lora_helper import set_single_lora, set_multi_lora
 
 # Define cache directory if needed, or rely on default huggingface_hub cache
 MODEL_CACHE = "./checkpoints/models/"  # Local cache for LoRA
@@ -59,11 +59,18 @@ class Predictor(BasePredictor):
 
         # --- Model Paths ---
         base_path = "black-forest-labs/FLUX.1-dev"
-        lora_repo_id = "Xiaojiu-Z/EasyControl"
-        lora_filename = "models/Ghibli.safetensors"
-        lora_local_dir = MODEL_CACHE
-        lora_full_path = os.path.join(lora_local_dir, lora_filename)
 
+        self.lora_dict = {}
+        try:
+            with open("loras.yaml", "r") as f:
+                self.lora_dict = yaml.load(f)
+            print(f"all loras: {self.lora_dict}")
+        except Exception as e:
+            print(f"Error loading loras.yaml: {e}")
+            raise
+
+        lora_local_dir = MODEL_CACHE
+        
         # --- Downlaod base model ---
         if not os.path.exists(base_path):
             # Thanks to [replicate/cog-flux](https://github.com/replicate/cog-flux)
@@ -75,18 +82,22 @@ class Predictor(BasePredictor):
         print(f"Ensuring LoRA directory exists: {lora_local_dir}")
         os.makedirs(lora_local_dir, exist_ok=True)
 
-        print(f"Downloading LoRA: {lora_filename} from {lora_repo_id}")
-        try:
-            hf_hub_download(
-                repo_id=lora_repo_id,
-                filename=lora_filename,
-                local_dir=lora_local_dir,
-                local_dir_use_symlinks=False,  # Recommended for Cog compatibility
-            )
-            print("LoRA download complete.")
-        except Exception as e:
-            print(f"Error downloading LoRA: {e}")
-            raise
+        for lora_name, lora_info in self.lora_dict.items():
+            lora_filename = lora_info["file"]
+            lora_repo_id = lora_info["repo_id"]
+            print(f"Downloading LoRA: {lora_filename} from {lora_repo_id}")
+            try:
+                hf_hub_download(
+                    repo_id=lora_repo_id,
+                    filename=lora_filename,
+                    local_dir=lora_local_dir,
+                    local_dir_use_symlinks=False,  # Recommended for Cog compatibility
+                )
+                print("LoRA download complete.")
+                self.lora_dict[lora_name]["full_path"] = os.path.join(lora_local_dir, lora_filename)
+            except Exception as e:
+                print(f"Error downloading LoRA: {e}")
+                raise
 
         # --- Load Base Model ---
         print(f"Loading base pipeline from {base_path}")
@@ -96,18 +107,6 @@ class Predictor(BasePredictor):
             base_path, subfolder="transformer", torch_dtype=torch.bfloat16
         )
         self.pipe.transformer = transformer
-
-        # --- Apply LoRA ---
-        print(f"Applying LoRA from {lora_full_path}")
-        try:
-            set_single_lora(
-                self.pipe.transformer, lora_full_path, lora_weights=[1.0], cond_size=512
-            )  # Ensure weight is float
-            print("LoRA applied successfully.")
-        except Exception as e:
-            print(f"Error applying LoRA: {e}")
-            # Decide if this should raise or continue without LoRA
-            raise
 
         # --- Move to Device ---
         print(f"Moving pipeline to {self.device}")
@@ -122,9 +121,8 @@ class Predictor(BasePredictor):
         self,
         prompt: str = Input(
             description="Enter a text prompt to guide image generation.",
-            default="Ghibli Studio style, Charming hand-drawn anime-style illustration",
+            default="",
         ),
-        spatial_img: Path = Input(description="Ghibli-style spatial control image."),
         height: int = Input(
             description="Set the height of the generated image (pixels).",
             ge=256,
@@ -142,11 +140,27 @@ class Predictor(BasePredictor):
         ),
         use_zero_init: bool = Input(description="Use CFG zero star.", default=True),
         zero_steps: int = Input(description="Zero init steps.", default=1, ge=0),
+        spatial_img: Path = Input(description= "spatial control image."),
         control_type: str = Input(
-            description="Control type (currently only Ghibli supported).",
-            choices=["Ghibli"],
-            default="Ghibli",
+            description="Control type (now only support two types, pose and mine_craft).",
+            choices=["mine_craft", "pose"],
+            default="mine_craft",
         ),
+        control_weight: float = Input(
+            description="Control weights (must correspond to the control type).",
+            default=1.0,
+        ),
+        extra_spatial_img: Path = Input(description= "extra spatial control image.", default=None),
+        extra_control_type: str = Input(
+            description="Extra control type (now only support two types, pose and mine_craft).",
+            choices=["mine_craft", "pose", ""],
+            default="",
+        ),
+        extra_control_weight: float = Input(
+            description="Extra control weights (must correspond to the extra control type).",
+            default=1.0,
+        ),
+
         output_format: str = Input(
             description="Choose the format of the output images.",
             choices=["png", "jpg", "webp"],
@@ -162,11 +176,51 @@ class Predictor(BasePredictor):
         """Run a single prediction on the model"""
         predict_start_time = time.time()
 
-        if control_type != "Ghibli":
-            print(
-                f"Warning: Control type '{control_type}' is not explicitly handled. Using Ghibli LoRA."
-            )
-            # Currently, only Ghibli LoRA is loaded in setup.
+        if extra_control_type and extra_spatial_img is None:
+            print(f"Extra control type must be provided with extra spatial image, now is {extra_control_type} and {extra_spatial_img}, please check your input")
+            raise ValueError(f"Extra control type must be provided with extra spatial image, now is {extra_control_type} and {extra_spatial_img}, please check your input")
+
+        if extra_control_type and extra_control_type not in self.lora_dict:
+            print(f"Extra control type '{extra_control_type}' not found in loras.yaml, can't support this control type")
+            raise ValueError(f"Extra control type '{extra_control_type}' not found in loras.yaml")
+
+        if control_type not in self.lora_dict:
+            print(f"Control type '{control_type}' not found in loras.yaml, can't support this control type")
+            raise ValueError(f"Control type '{control_type}' not found in loras.yaml")
+        
+        # --- Apply LoRA ---
+        control_type_num = 0
+        if control_type and not extra_control_type:
+            lora_full_path = self.lora_dict[control_type]["full_path"]
+            lora_weights = [control_weight] if control_weight else [1.0]
+            try:
+                set_single_lora(
+                    self.pipe.transformer, lora_full_path, lora_weights=lora_weights, cond_size=512
+                )  # Ensure weight is float
+                print("LoRA applied successfully.")
+                control_type_num = 1
+            except Exception as e:
+                print(f"Error applying LoRA: {e}")
+                # Decide if this should raise or continue without LoRA
+                raise
+        elif control_type and extra_control_type:
+            lora_full_paths = [self.lora_dict[control_type]["full_path"] for control_type in [control_type, extra_control_type]]
+            lora_weights =[ w for w in [control_weight, extra_control_weight] if w]
+            if len(lora_weights) < 2:
+                print(f"Warning: Not enough control weights provided, add default weights [1.0]")
+                lora_weights = lora_weights + [1.0] * (2 - len(lora_weights))
+            try:
+                set_multi_lora(
+                    self.pipe.transformer, lora_full_paths, lora_weights=lora_weights, cond_size=512
+                )  # Ensure weight is float
+                print("Multi LoRA applied successfully.")
+                control_type_num = 2
+            except Exception as e:
+                print(f"Error applying LoRA: {e}")
+                raise
+
+        self.pipe.to(self.device)
+        print("Pipeline moved to device.")
 
         # --- Handle Seeds ---
         if seed == -1:
@@ -174,15 +228,22 @@ class Predictor(BasePredictor):
         print(f"Using seed: {seed}")
 
         # --- Load Spatial Image ---
-        try:
-            loaded_spatial_img = Image.open(str(spatial_img)).convert("RGB")
-            spatial_imgs_list = [loaded_spatial_img]
-            print(f"Loaded spatial image from: {spatial_img}")
-        except Exception as e:
-            print(f"Error loading spatial image: {e}")
-            raise ValueError(
-                f"Could not load spatial image from path: {spatial_img}"
-            ) from e
+        spatial_imgs_list = []
+        spatial_imgs = [img for img in [spatial_img, extra_spatial_img] if img]
+        if len(spatial_imgs) != control_type_num:
+            print(f"spatial image number {len(spatial_imgs)} is not equal to control type number {control_type_num}, please check your input")
+            raise ValueError(f"spatial image number {len(spatial_imgs)} is not equal to control type number {control_type_num}, please check your input")
+
+        for spatial_img in spatial_imgs:
+            try:
+                loaded_spatial_img = Image.open(str(spatial_img)).convert("RGB")
+                spatial_imgs_list.append(loaded_spatial_img)
+                print(f"Loaded spatial image from: {spatial_img}")
+            except Exception as e:
+                print(f"Error loading spatial image: {e}")
+                raise ValueError(
+                    f"Could not load spatial image from path: {spatial_img}"
+                ) from e
 
         output_paths = []
 
